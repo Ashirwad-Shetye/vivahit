d28da38945dca93c4f0bea3acb020fb570e9e23d
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-07-08 16:26:02 +0530

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Logo from "../assets/logo/logo.png";
 import { RxDashboard } from "react-icons/rx";
-import { TbSettings2 } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
 import { SlLocationPin } from "react-icons/sl";
 import { Button, buttonVariants } from "./ui/Button";
+import { PiPresentationChart } from "react-icons/pi";
+import { BsCalendarDate } from "react-icons/bs";
+import { LiaDoorOpenSolid } from "react-icons/lia";
 
 function Nav() {
   const [isFocused, setIsFocused] = useState(true);
@@ -11,13 +14,13 @@ function Nav() {
     setIsFocused(isFocused);
   };
   return (
-    <div className="w-32 h-screen">
+    <div className="w-32 relative h-screen">
       <img
         src={Logo}
         alt="weather logo"
         className="w-12 mx-auto py-10 rounded-full"
       />
-      <div className="mx-auto mt-24 w-fit flex flex-col space-y-16">
+      <div className="mx-auto mt-4 h-80 w-fit flex flex-col justify-between">
         <Button
           className={buttonVariants({ variant: "navFocus" })}
           onClick={handleButtonFocus}
@@ -25,10 +28,21 @@ function Nav() {
           <RxDashboard />
         </Button>
         <Button>
+          <PiPresentationChart />
+        </Button>
+        <Button>
           <SlLocationPin />
         </Button>
         <Button>
-          <TbSettings2 />
+          <BsCalendarDate />
+        </Button>
+        <Button>
+          <IoSettingsOutline />
+        </Button>
+      </div>
+      <div className="absolute bottom-16 w-full flex justify-center">
+        <Button>
+          <LiaDoorOpenSolid className="text-2xl" />
         </Button>
       </div>
     </div>
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Weather() {
-  return <div className="border rounded-2xl">Weather</div>;
+  return (
+    <div className="border relative overflow-hidden rounded-2xl">
+      <div className="grid grid-rows-3 w-full px-4 h-full py-4">
+        <div>
+          <h1>Weather</h1>
+          <p className="text-sm">what's the weather.</p>
+        </div>
+        <div>
+          <div className="flex space-x-4">
+            <h1>22 c</h1>
+            <h2>11 c</h2>
+          </div>
+          <p className="text-sm">Partly Cloudy</p>
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="text-white bg-accentSecondary rounded-xl flex flex-col justify-center items-center">
+            <p>Pressure</p>
+            <h1>800mb</h1>
+          </div>
+          <div className="bg-accentGreen rounded-xl flex flex-col justify-center items-center">
+            <p>Visibility</p>
+            <h1>4.3 km</h1>
+          </div>
+          <div className="border rounded-xl flex flex-col justify-center items-center">
+            <p>Humidity</p>
+            <h1>87%</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Weather;
