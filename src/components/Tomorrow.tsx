56fe36378a17c790406a2bd84a1eb0ed87e16ff3
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-07-08 18:51:43 +0530

    Tomorrow UI

--- src/components/Tomorrow.tsx
+++ src/components/Tomorrow.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Tomorrow() {
-  return <div className="bg-blue-300 col-span-4 rounded-2xl">Tomorrow</div>;
+  return (
+    <div className="bg-accentGreen/80 text-accentSecondary relative col-span-4 rounded-2xl overflow-hidden">
+      <div className="px-10 py-10 h-full flex flex-col justify-between">
+        <div>
+          <h1>Tomorrow</h1>
+        </div>
+        <div className="space-y-1">
+          <h1 className="font-semibold text-4xl">22° C</h1>
+          <p className="text-sm">Partly Cloudy</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tomorrow;
