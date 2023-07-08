6e5612d0915a60deb34070ebf99923529cdb0cd9
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-07-08 19:02:04 +0530

    Today UI

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Button } from "./ui/Button";
+import { BsWind, BsThermometerHalf, BsUmbrella } from "react-icons/bs";
 
 function Today() {
-  return <div className=" bg-slate-300 col-span-8 rounded-2xl">Today</div>;
+  return (
+    <div className=" bg-slate-300 col-span-8 text-accentSecondary rounded-2xl">
+      <div className="p-8">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">
+            How's the <br /> temperature today?
+          </h1>
+          <div className="space-x-2">
+            <Button variant={"navFocus"}>
+              <BsThermometerHalf />
+            </Button>
+            <Button>
+              <BsUmbrella />
+            </Button>
+            <Button>
+              <BsWind />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Today;
