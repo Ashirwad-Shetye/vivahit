a4be88e1935a4baaf4c87356734c173ecfbabede
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-07-10 00:57:24 +0530

    fix:apiCalls

--- src/api/apiCalls.ts
+++ src/api/apiCalls.ts
@@ -1,6 +1,18 @@
-import { useWeatherStore, useLocationStore } from "../service/store";
+import axios from "axios"
+import { WeatherData, LocationData } from "../lib/types";
+import { OW_KEY, OW_API_URL } from "./constants";
 
-const weatherData = useWeatherStore((state) => state.searchData);
-const updateWeather = useWeatherStore((state) => state.updateWeatherData);
-const locationData = useLocationStore((state) => state.location);
-const updatelocation = useLocationStore((state) => state.updateLocation);
+  
+export const fetchCurrentWeatherData = async (location: LocationData): Promise<WeatherData> => {
+    const [lat, lon] = location.location.value.split(" ");
+    const response = await axios.get(`${OW_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OW_KEY}&units=metric`);
+    const data = response.data;
+    return data 
+};
+
+export const fetchForecastData = async (location: LocationData): Promise<WeatherData> => {
+    const [lat, lon] = location.location.value.split(" ");
+    const response = await axios.get(`${OW_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OW_KEY}&units=metric`);
+    const data = response.data;
+    return data
+};
--- src/api/constants.ts
+++ src/api/constants.ts
@@ -8,7 +8,7 @@ export const geoApiOptions = {
 
 export const GEO_API_URL : string = import.meta.env.VITE_GEO_API_URL
 
-export const WEATHER_API_URL = import.meta.env.VITE_OW_API_URL
+export const OW_API_URL = import.meta.env.VITE_OW_API_URL
 
 export const OW_KEY = import.meta.env.VITE_OW_API_KEY
 
